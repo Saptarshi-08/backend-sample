fd92fcb8ec435483cf7869e8d72fa1f786648d02
Saptarshi Datta <saptarshidatta08@gmail.com>
2025-04-27 19:52:49 +0530

    forked

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "https://hermesmer.vercel.app",
-      "*",
+      "https://hermes-1-jet.vercel.app",
       "http://localhost:3001",
       "http://localhost:3000",
     ],
